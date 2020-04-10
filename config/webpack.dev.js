140944d7514172f7cc579f27d215bfe3958415f8
Spyros <dev@spyros.uk>
2020-04-10 17:20:49 +0100

    Fixes HMRC

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -76,6 +76,7 @@ module.exports = merge.smart(common, {
     ]
   },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
       "process.env": {
         NODE_ENV: JSON.stringify("development")
